10b6568161280be22c434c0b867aefa48da5caba
Ezekiel Badowski <ezekielbadowski@gmail.com>
2019-03-28 21:05:24 -0500

    hint button update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,68 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //creates arrayed list of words and hints
     var animals = [
-        ["alligator", "_images/animals/alligator.jpg"], 
-        ["bear", "_images/animals/bear.jpg"],  
-        ["bird", "_images/animals/bird.png"], 
-        ["camel", "_images/animals/camel.png"], 
-        ["cat", "_images/animals/cat.png"], 
-        ["cheetah", "_images/animals/cheetah.jpg"], 
-        ["chicken", "_images/animals/chicken.png"], 
-        ["cow", "_images/animals/cow.png"], 
-        ["crocodile", "_images/animals/crocodile.jpg"], 
-        ["deer", "_images/animals/deer.jpg"], 
-        ["dog", "_images/animals/dog.jpg"], 
-        ["dolphin", "_images/animals/dolphin.jpg"], 
-        ["duck", "_images/animals/duck.jpg"], 
-        ["elephant", "_images/animals/elephant.jpg"], 
-        ["fish", "_images/animals/fish.jpg"], 
-        ["fox", "_images/animals/fox.png"], 
-        ["frog", "_images/animals/frog.png"], 
-        ["giraffe", "_images/animals/giraffe.png"], 
-        ["goat", "_images/animals/goat.png"], 
-        ["hippopotamus", "_images/animals/hippopotamus.png"], 
-        ["horse", "_images/animals/horse.jpg"], 
-        ["kangaroo", "_images/animals/kangaroo.png"], 
-        ["lion", "_images/animals/lion.png"],  
-        ["monkey", "_images/animals/monkey.jpg"], 
-        ["octopus", "_images/animals/octopus.jpg"], 
-        ["owl", "_images/animals/owl.jpg"], 
-        ["panda", "_images/animals/panda.jpg"], 
-        ["pig", "_images/animals/pig.jpg"],  
-        ["rabbit", "_images/animals/rabbit.jpg"], 
-        ["rat", "_images/animals/rat.jpg"], 
+        ["alligator", "_images/animals/alligator.jpg"],
+        ["bear", "_images/animals/bear.jpg"],
+        ["bird", "_images/animals/bird.png"],
+        ["camel", "_images/animals/camel.png"],
+        ["cat", "_images/animals/cat.png"],
+        ["cheetah", "_images/animals/cheetah.jpg"],
+        ["chicken", "_images/animals/chicken.png"],
+        ["cow", "_images/animals/cow.png"],
+        ["crocodile", "_images/animals/crocodile.jpg"],
+        ["deer", "_images/animals/deer.jpg"],
+        ["dog", "_images/animals/dog.jpg"],
+        ["dolphin", "_images/animals/dolphin.jpg"],
+        ["duck", "_images/animals/duck.jpg"],
+        ["elephant", "_images/animals/elephant.jpg"],
+        ["fish", "_images/animals/fish.jpg"],
+        ["fox", "_images/animals/fox.png"],
+        ["frog", "_images/animals/frog.png"],
+        ["giraffe", "_images/animals/giraffe.png"],
+        ["goat", "_images/animals/goat.png"],
+        ["hippopotamus", "_images/animals/hippopotamus.png"],
+        ["horse", "_images/animals/horse.jpg"],
+        ["kangaroo", "_images/animals/kangaroo.png"],
+        ["lion", "_images/animals/lion.png"],
+        ["monkey", "_images/animals/monkey.jpg"],
+        ["octopus", "_images/animals/octopus.jpg"],
+        ["owl", "_images/animals/owl.jpg"],
+        ["panda", "_images/animals/panda.jpg"],
+        ["pig", "_images/animals/pig.jpg"],
+        ["rabbit", "_images/animals/rabbit.jpg"],
+        ["rat", "_images/animals/rat.jpg"],
         ["scorpion", "_images/animals/scorpion.png"],
-        ["shark", "_images/animals/shark.jpg"], 
-        ["sheep", "_images/animals/sheep.jpg"], 
-        ["snail", "_images/animals/snail.jpg"], 
-        ["snake", "_images/animals/snake.jpg"], 
-        ["squirrel", "_images/animals/squirrel.png"], 
-        ["tiger", "_images/animals/tiger.png"], 
-        ["turtle", "_images/animals/turtle.png"], 
-        ["wolf", "_images/animals/wolf.png"], 
+        ["shark", "_images/animals/shark.jpg"],
+        ["sheep", "_images/animals/sheep.jpg"],
+        ["snail", "_images/animals/snail.jpg"],
+        ["snake", "_images/animals/snake.jpg"],
+        ["squirrel", "_images/animals/squirrel.png"],
+        ["tiger", "_images/animals/tiger.png"],
+        ["turtle", "_images/animals/turtle.png"],
+        ["wolf", "_images/animals/wolf.png"],
         ["zebra", "_images/animals/zebra.jpg"]
     ];
-    
 
-   
+
+
     //initial key click to clear instructions and show page
     document.onkeyup = function (event) {
 
         //clears instructions from page
         $("#instructions").empty();
 
-        var packet = animals[Math.floor(Math.random()*animals.length)];
+        var packet = animals[Math.floor(Math.random() * animals.length)];
         var word = packet[0];
         var guesses = [];
         var numCorrect = 0;
         var wins = 0;
         var losses = 0;
         var guessesRemaining = 7;
+        var clickedHint = false;
 
         //INITIAL PAGE BUILD
         buildPage();
-        
+
 
         //Game starts with user inputing letter
         document.onkeyup = function (event) {
@@ -70,64 +71,75 @@ $(document).ready(function() {
             guesses.push(entry);
 
             //update incorrect guesses
-            if (!word.includes(entry) && guessesRemaining > 0){
+            if (!word.includes(entry) && guessesRemaining > 0) {
                 guessesRemaining--;
             }
-            else if (!word.includes(entry)){
+            else if (!word.includes(entry)) {
                 loseCondition();
             }
-            if(word.includes(entry)){
+            if (word.includes(entry)) {
                 //checks for repeating letters
-                for(var i = 0; i < word.length; i++){
-                    if(word[i] == entry){
+                for (var i = 0; i < word.length; i++) {
+                    if (word[i] == entry) {
                         numCorrect++;
                     }
                 }
             }
-            if(numCorrect == word.length){
+            if (numCorrect == word.length) {
                 winCondition();
             }
-            //update correct guess using show word function
 
+            //NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             //onclick hint button display image using link in nested array
+            $(".hint-button").on("click", function () {
+                clickedHint = true;
+                $("#hint").empty();
+                $('#hint').prepend($('<img>',{id:'theImg',src: String(packet[1])}))
+                //$("#hint").append("<img src=\"" + packet[1] + "\">");
+            });
 
-            //win conditions
-                // clear hint image
+
             //update page
             buildPage();
         }
 
         //builds word text with dashes or guessed letters
-        function buildWord(word, guesses){
+        function buildWord(word, guesses) {
             //clear word before adding to it
             $("#word").empty();
-            for(var i = 0; i<word.length; i++){
+            for (var i = 0; i < word.length; i++) {
                 //check guessed letters adding either the letters guessed or an underscore
-                if(guesses.includes(word[i])){
+                if (guesses.includes(word[i])) {
                     $("#word").append(" " + word[i] + " ");
                 }
-                else{
+                else {
                     $("#word").append(" _ ");
                 }
             }
         }
-        function buildPage(){
+        function buildPage() {
             //Clear page before building
             clearPage();
             //build page
             $("#current-word").append("Current Word");
-            buildWord(word,guesses);
+            buildWord(word, guesses);
             $("#header-guess").append("Guesses Remaining:");
             $("#num-guessed").append(String(guessesRemaining));
             $("#header-guessed").append("Guessed Letters:");
-            for(var i = 0; i<guesses.length; i++){
+            for (var i = 0; i < guesses.length; i++) {
                 $("#guessed-letters").append(guesses[i] + " ");
             }
-            
+
             $("#wins-losses").append("Wins: " + String(wins) + " || " + "Losses: " + String(losses));
-            //ADD HINT BUTTON
+            //only add hint button if it hasn't been clicked that round
+            if (!clickedHint) {
+                $("#hint").empty();
+                var hintButton = $("<button class=\"hint-button\">");
+                $(hintButton).text("HINT");
+                $("#hint").append(hintButton);
+            }
         }
-        function clearPage(){
+        function clearPage() {
             $("#current-word").empty();
             $("#header-guess").empty();
             $("#num-guessed").empty();
@@ -135,23 +147,23 @@ $(document).ready(function() {
             $("#guessed-letters").empty();
             $("#wins-losses").empty();
         }
-        function loseCondition(){
+        function loseCondition() {
             losses++;
             guessesRemaining = 7;
-            packet = animals[Math.floor(Math.random()*animals.length)];
+            packet = animals[Math.floor(Math.random() * animals.length)];
             word = packet[0];
             guesses = [];
             numCorrect = 0;
-            //clear hint image
+            clickedHint = false;
         }
-        function winCondition(){
+        function winCondition() {
             wins++;
             guessesRemaining = 7;
-            packet = animals[Math.floor(Math.random()*animals.length)];
+            packet = animals[Math.floor(Math.random() * animals.length)];
             word = packet[0];
             guesses = [];
             numCorrect = 0;
-            //clear hint image
+            clickedHint = false;
         }
     }
 });

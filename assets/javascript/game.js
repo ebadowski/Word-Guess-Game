70f57c7828e89c02f415c6c48a303584dffc1a2b
Ezekiel Badowski <ezekielbadowski@gmail.com>
2019-03-30 14:23:15 -0500

    fixed hint and numCorrect bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,103 +42,70 @@ $(document).ready(function () {
         ["wolf", "assets/images/animals/wolf.png"],
         ["zebra", "assets/images/animals/zebra.jpg"]
     ];
+    $(".start-button").text("Click to start!");
 
-//clickHint();
 
-    //initial key click to clear instructions and show page
+    startGame();
+
+    //Game starts with user inputing letter
     document.onkeyup = function (event) {
+        //add keystroke to guess list
+        var entry = event.key;
+        if (!guesses.includes(entry)) {
+            guesses.push(entry);
+        }else{entry = "";}
 
+        //update incorrect guesses
+        if (!word.includes(entry) && guessesRemaining > 0) {
+            guessesRemaining--;
+        }
+        else if (!word.includes(entry)) {
+            loseCondition();
+        }
+        if (word.includes(entry)) {
+            //checks for repeating letters
+            for (var i = 0; i < word.length; i++) {
+                if (word[i] == entry) {
+                    numCorrect++;
+                }
+            }
+        }
+        if (numCorrect == word.length) {
+            winCondition();
+        }
+
+        //update page
+        buildPage();
+    }
+
+
+
+
+
+});
+function startGame() {
+    $(".start-button").on("click", function () {
         //clears instructions from page
         $("#instructions").empty();
 
-         packet = animals[Math.floor(Math.random() * animals.length)];
-         word = packet[0];
-         guesses = [];
-         numCorrect = 0;
-         wins = 0;
-         losses = 0;
-         guessesRemaining = 7;
-         clickedHint = false;
+        packet = animals[Math.floor(Math.random() * animals.length)];
+        word = packet[0];
+        guesses = [];
+        numCorrect = 0;
+        wins = 0;
+        losses = 0;
+        guessesRemaining = 7;
+        clickedHint = false;
 
         //INITIAL PAGE BUILD
         buildPage();
 
-        //clickHint();
-        
 
-        //Game starts with user inputing letter
-        document.onkeyup = function (event) {
-            //add keystroke to guess list
-            var entry = event.key;
-            guesses.push(entry);
+        //This click hint is working
+        clickHint();
+    });
 
-            //update incorrect guesses
-            if (!word.includes(entry) && guessesRemaining > 0) {
-                guessesRemaining--;
-            }
-            else if (!word.includes(entry)) {
-                loseCondition();
-            }
-            if (word.includes(entry)) {
-                //checks for repeating letters
-                for (var i = 0; i < word.length; i++) {
-                    if (word[i] == entry) {
-                        numCorrect++;
-                    }
-                }
-            }
-            if (numCorrect == word.length) {
-                winCondition();
-            }
-
-            
-
-            //clickHint();
-
-            //update page
-            buildPage();
-        }
-
-        //builds word text with dashes or guessed letters
-        function buildWord(word, guesses) {
-            //clear word before adding to it
-            $("#word").empty();
-            for (var i = 0; i < word.length; i++) {
-                //check guessed letters adding either the letters guessed or an underscore
-                if (guesses.includes(word[i])) {
-                    $("#word").append(" " + word[i] + " ");
-                }
-                else {
-                    $("#word").append(" _ ");
-                }
-            }
-        }
-        function buildPage() {
-            //Clear page before building
-            clearPage();
-            //build page
-            $("#current-word").append("Current Word");
-            buildWord(word, guesses);
-            $("#header-guess").append("Guesses Remaining:");
-            $("#num-guessed").append(String(guessesRemaining));
-            $("#header-guessed").append("Guessed Letters:");
-            for (var i = 0; i < guesses.length; i++) {
-                $("#guessed-letters").append(guesses[i] + " ");
-            }
-
-            $("#wins-losses").append("Wins: " + String(wins) + " || " + "Losses: " + String(losses));
-            //only add hint button if it hasn't been clicked that round
-            //clickHint()
-            if (!clickedHint) {
-                $("#hint").empty();
-                var hintButton = $("<button class=\"hint-button\" function=\"clickHint()\">");
-                $(hintButton).text("HINT");
-                $("#hint").append(hintButton);
-            }
-        }
-        
-    }
-});
+}
 function clearPage() {
     $("#current-word").empty();
     $("#header-guess").empty();
@@ -165,13 +132,51 @@ function winCondition() {
     numCorrect = 0;
     clickedHint = false;
 }
+
 function clickHint() {
-    //NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     //onclick hint button display image using link in nested array
     $(".hint-button").on("click", function () {
         clickedHint = true;
+        console.log("clicked hint");
         $("#hint").empty();
         $('#hint').prepend($('<img>', { id: 'theImg', src: String(packet[1]) }))
         //$("#hint").append("<img src=\"" + packet[1] + "\">");
     });
+}
+function buildWord(word, guesses) {
+    //clear word before adding to it
+    $("#word").empty();
+    for (var i = 0; i < word.length; i++) {
+        //check guessed letters adding either the letters guessed or an underscore
+        if (guesses.includes(word[i])) {
+            $("#word").append(" " + word[i] + " ");
+        }
+        else {
+            $("#word").append(" _ ");
+        }
+    }
+}
+function buildPage() {
+    //Clear page before building
+    clearPage();
+    //build page
+    $("#current-word").append("Current Word");
+    buildWord(word, guesses);
+    $("#header-guess").append("Guesses Remaining:");
+    $("#num-guessed").append(String(guessesRemaining));
+    $("#header-guessed").append("Guessed Letters:");
+    for (var i = 0; i < guesses.length; i++) {
+        $("#guessed-letters").append(guesses[i] + " ");
+    }
+
+    $("#wins-losses").append("Wins: " + String(wins) + " || " + "Losses: " + String(losses));
+
+    //only add hint button if it hasn't been clicked that round
+    if (!clickedHint) {
+        $("#hint").empty();
+        var hintButton = $("<button class=\"hint-button\" function=\"clickHint()\">");
+        $(hintButton).text("HINT");
+        $("#hint").append(hintButton);
+        clickHint();
+    }
 }
